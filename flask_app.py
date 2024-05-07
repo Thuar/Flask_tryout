ef3d9cc66fa2ebdafcbdb943fce90308bd288f70
Sven Van Dorst <sven.van.dorst@telenet.be>
2024-05-07 23:23:06 +0200

    nog een vergeten aanpassing

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
+app=Flask(__name__)
+
 @app.route('/')
 def home():
         return "<p>Dit is een test</p>"
+
+if __name__ == '__main__':
+        app.run(port=5000,debug=True)
